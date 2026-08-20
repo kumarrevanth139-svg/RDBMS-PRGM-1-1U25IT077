cdde00118d61f783c7ecf9d48e00798fe7448f63
kumarrevanth139-svg <kumarrevanth139@gmail.com>
2026-08-20 13:43:38 +0530

    RDBMS PRGM-1 1U25IT077

--- solution.sql
+++ solution.sql
@@ -1,1 +1,8 @@
-
+CREATE DATABASE universityDB;
+USE universityDB;
+CREATE TABLE dapartment(
+  departmentID numberic(5)PRIMARY KEY,
+  departmentName varchar(20) NOT NULL,
+  HOD varhar(20)NOT NULL
+);
+ desc dapartment;
