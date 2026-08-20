3dd0c3d4fe7b15d7936e43bfc829de9086c2de6a
kumarrevanth139-svg <kumarrevanth139@gmail.com>
2026-08-20 14:12:41 +0530

    RDBMS PRGM-1 1U25IT077

--- solution.sql
+++ solution.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
-CREATE TABLE Department (
-  departmentID INT(5) PRIMARY KEY,
-  departmentName VARCHAR(20),
+CREATE TABLE Department(
+  DepartmentID INT(5) PRIMARY KEY,
+  DepartmentName VARCHAR(20),
   HOD VARCHAR(20)
 );
  DESC department;
